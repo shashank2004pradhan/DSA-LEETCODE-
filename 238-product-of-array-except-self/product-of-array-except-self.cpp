d27ec9c99d01f42b1b92c2c78d61da2db0d78035
Shashank Sekhar Pradhan <119575109+shashank2004pradhan@users.noreply.github.com>
2025-06-25 11:19:49 +0530

    Time: 2 ms (44.32%) | Memory: 40.1 MB (94.48%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,21 +4,23 @@ public:
 
         int n = nums.size();
         vector<int> ans(n,1);
-        vector<int> prefix(n,1);
-        vector<int> suffix(n,1);
+        // vector<int> prefix(n,1);
+        // vector<int> suffix(n,1);
 
-        //prefix o(n)
+        //prefix o(n) => ans
         for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]*nums[i-1];
+            ans[i]=ans[i-1]*nums[i-1];
         }
-        //suffix o(n)
+
+        int suffix =1;
+        //suffix o(n) 
          for(int i=n-2;i>=0;i--){
-           suffix[i]=suffix[i+1]*nums[i+1];
+            suffix = suffix *nums[i+1];
+            ans[i] = ans[i] * suffix;
+
+           
         }
-        //ans
-        for(int i=0;i<n;i++){
-            ans[i]=prefix[i]*suffix[i];
-        }
+        
 
         
               
