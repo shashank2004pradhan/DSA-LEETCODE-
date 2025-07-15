2b66679cd4423d2c858133e66fe8191999313fdd
Shashank Sekhar Pradhan <119575109+shashank2004pradhan@users.noreply.github.com>
2025-07-15 11:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.32%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& a) {
-
-    int piv = -1;
-    int n= a.size();
-    //find pivot
-    for(int i=n-2;i>=0;i--){
-        if(a[i] < a[i+1]){
-            piv =i;
-            break;
+        int piv= -1;
+        int n = a.size();
+         //find pivot 
+        for(int i=n-2;i>=0;i--){
+            if(a[i]<a[i+1]){
+                piv = i;
+                break;
+            }
         }
-    }
-
-    //edge case:  if arr is decreasing 
-    if(piv==-1){
-        reverse(a.begin(),a.end());
-        return;
-    }
-
-    //find right-most element and if its greater than pivot then swap
-    for(int i=n-1;i>=0;i--){
-        if(a[i]>a[piv]){
-            swap(a[i],a[piv]);
-            break;
+        //edgecase
+        if(piv==-1){
+            reverse(a.begin(),a.end());
+            return;
         }
-    }
-// reverse the piv+1 to n-1
-    int i = piv+1;
-    int j = n-1;
 
-    while(i<=j){
-        swap(a[i],a[j]);
-        i++;
-        j--;
-    }
+        //find right most el and if its greater than pivot ,swap em
+        for(int i=n-1;i>=0;i--){
+            if(a[i]>a[piv]){
+                swap(a[i],a[piv]);
+                break;
+            }
+        }
 
+        //reverse from piv+1 to n-1
+        int i=piv+1;
+        int j=n-1;
+        while(i<=j){
+            swap(a[i],a[j]);
+            i++;
+            j--;
+        }
         
     }
 };
