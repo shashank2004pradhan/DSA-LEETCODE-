239025b7b6662cf2cff9d324d72f5af0abb8a65d
Shashank Sekhar Pradhan <119575109+shashank2004pradhan@users.noreply.github.com>
2025-07-22 13:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (16.02%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n = chars.size();
-        int idx=0;
+        int n = chars.size();       // Total number of characters
+        int writeIndex = 0;         // Position to write the compressed characters
 
-        for(int i=0;i<n;i++){
-            int cnt = 0; // count = cnt
-            char ch = chars[i]; //current char
+        for (int i = 0; i < n; i++) {
+            int count = 0;          // To count how many times current character repeats
+            char currentChar = chars[i];  // Get the current character
 
-            while(i<n && chars[i] == ch){
-                cnt++;
+            // Count how many times currentChar appears consecutively
+            while (i < n && chars[i] == currentChar) {
+                count++;
                 i++;
             }
 
-            if(cnt == 1) {
-                chars[idx++] = ch;
-            }else{
-                chars[idx++] = ch;
-                string str = to_string(cnt);
-                for(char dig: str){
-                    chars[idx++]=dig;
-                }
-
-            }
+            // Since the outer loop also does i++, we decrease it here to correct position
             i--;
 
+            // Write the character once
+            chars[writeIndex] = currentChar;
+            writeIndex = writeIndex + 1;
 
+            // If count > 1, write the digits of the count one by one
+            if (count > 1) {
+                string countStr = to_string(count);  // Convert count to string
 
-
-
-
+                // Write each digit of the count separately
+                for (int j = 0; j < countStr.length(); j++) {
+                    chars[writeIndex] = countStr[j];
+                    writeIndex = writeIndex + 1;
+                }
+            }
         }
-        chars.resize(idx);
-        return idx;
-        
+
+        // Resize the chars vector to the final compressed length
+        chars.resize(writeIndex);
+
+        // Return the length of the compressed vector
+        return writeIndex;
     }
 };
