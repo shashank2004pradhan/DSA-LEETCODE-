7ffe3e1874969e3e3798f8d0069694c925193ad4
Shashank Sekhar Pradhan <119575109+shashank2004pradhan@users.noreply.github.com>
2025-07-23 09:12:07 +0530

    Update remove-all-occurrences-of-a-substring.cpp

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -3,7 +3,10 @@ public:
     string removeOccurrences(string s, string part) {
         while( s.length()>0 && s.find(part) < s.length() ){
             
-           //s.erase(i, len)  s.erase(s.find(part),part.length());
+           //s.erase(i, len)  
+
+
+s.erase(s.find(part),part.length());
         }
         return s;
         
