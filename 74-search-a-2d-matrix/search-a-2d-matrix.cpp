6dfd280f9b38df26dedb4b76e560ff6ad2fa2e8f
Shashank Sekhar Pradhan <119575109+shashank2004pradhan@users.noreply.github.com>
2025-08-03 17:59:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.14%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
-    bool search_row(vector<vector<int>>& mat, int tar, int row){
-        int n = mat[0].size();
-        int s = 0;
-        int e = n-1;
-
-        while(s<=e){
-            int mid = s +(e-s)/2 ;
+    bool row_search(vector<vector<int>>& mat, int tar,int row){
+        //search in row
+         int n = mat[0].size();
+         int s = 0, e =n-1;
+         while(s<=e){
+            int mid = s + (e-s)/2;
             if(tar == mat[row][mid]){
                 return true;
-            }else if(tar > mat[row][mid]){
+            }else if( tar > mat[row][mid]){
                 s = mid +1;
             }else{
-                e = mid -1;
+                e = mid-1;
             }
 
-        }
-        return false;
-
+         }
+         return false;
 
 
     };
+
+
+
 
 
     bool searchMatrix(vector<vector<int>>& mat, int tar) {
         int m = mat.size();
         int n = mat[0].size();
 
-        //search the right row
-        int s =0, e =m-1;
+    //choose right row    
+        int s =0;
+        int e = m-1;
         while(s<=e){
             int mid = s +(e-s)/2;
-
-            if( tar >= mat[mid][0] && tar <= mat[mid][n-1] ){
-                return search_row(mat,tar,mid);
-            }else if(tar > mat[mid][n-1]){
+            if(tar >= mat[mid][0] && tar <= mat[mid][n-1]){
+                return row_search(mat,tar,mid);
+            }else if( tar > mat[mid][n-1] ){
                 s = mid +1;
             }else{
-                e = mid-1;
+                e = mid -1;
             }
-
-
-
-
-
         }
         return false;
-
         
     }
 };
